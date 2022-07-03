2fa4a538e5ede50f88e6c655b376704d8b7750cc
Bruno Cesar <bruno_cco@hotmail.com>
2022-07-03 17:18:44 -0300

    Criando projeto

---
+++ monitore/src/main/java/com/project/monitore/MonitoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.monitore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoreApplication.class, args);
+	}
+
+}
---
+++ monitore/src/test/java/com/project/monitore/MonitoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.monitore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
